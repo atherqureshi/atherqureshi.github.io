e70c64f6635646627468dee90c8a1aee3a8e6005
atherqureshi <atherqureshi07@gmail.com>
2017-08-17 17:51:41 -0400

    added interactive graph to page underneath original graph

--- Thesis_help/offline thesis work/test.py
+++ Thesis_help/offline thesis work/test.py
@@ -30,9 +30,10 @@ def rules(clf, features, labels, node_index=0):
         count_labels = zip(clf.tree_.value[node_index, 0], labels)
         node['name'] = ', '.join(('{} of {}'.format(int(count), label)
                                   for count, label in count_labels))
-    else:
+    else: #non leaf / so parent
         feature = features[clf.tree_.feature[node_index]]
         threshold = clf.tree_.threshold[node_index]
+        threshold = str(round(threshold,2))
         node['name'] = '{} > {}'.format(feature, threshold)
         left_index = clf.tree_.children_left[node_index]
         right_index = clf.tree_.children_right[node_index]
